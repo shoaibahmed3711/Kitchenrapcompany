7b54161c5ab2862e03a64c670aab127901a278d8
Shoaib Ahmed <shoaibraza6711@gmail.com>
2024-05-19 13:04:12 +0500

    Push

--- src/appwrite/configuration.js
+++ src/appwrite/configuration.js
@@ -78,8 +78,51 @@ export class Services{
         }
     }
 
-    async getPost(queries){
+    async getPost(queries = [Query.equal("status", "active")]){
+        try {
+            return await this.databases.listDocuments(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                queries,
+                
+            )
+        } catch (error) {
+            console.log('Appwrite services :: createPost :: error', error);
+            return false
+        }
+    }
 
+    async uploadFile(file){
+        try {
+            return await this.bucket.createFile(
+                conf.appwriteBucketId,
+                ID.unique(),
+                file
+            )
+        } catch (error) {
+            console.log('Appwrite services :: createPost :: error', error);
+            return false
+        }
+    }
+
+    async deleteFile(fileId){
+        try {
+            return await this.bucket.deleteFile(
+                conf.appwriteBucketId,
+                fileId
+            )
+            return true
+        } catch (error) {
+            console.log('Appwrite services :: createPost :: error', error);
+            return false
+        }
+    }
+
+    getFilePreview(fileId){
+        return this.bucket.getFilePreview(
+            conf.appwriteBucketId,
+            fileId
+        )
     }
 }
 
